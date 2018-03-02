1c9579b99f7de00bc4168bfe7a3db348b76d5ce8
josh-taylor-k3 <this14hawk@gmail.com>
2018-03-02 14:59:07 +0100

    route productFournisseur GET

--- src/Controller/FournisseurController.php
+++ src/Controller/FournisseurController.php
@@ -47,8 +47,6 @@ class FournisseurController extends Controller
 
     }
 
-
-
     /**
      * @Route("/fournisseur/{id}", name="fournisseur")
      */
@@ -86,4 +84,33 @@ class FournisseurController extends Controller
 
 
     }
+
+
+    /**
+     * @Route("/fournisseur/{id}/products", name="fournisseur_products")
+     */
+    public function getProductsFromFournisseur(Request $request , ApiKeyAuth $auth, Connection $connection, HelperService $helper, $id)
+    {
+        header("Access-Control-Allow-Origin: *");
+        $key = $request->headers->get('X-ac-key');
+        if (isset($key) && $auth->grant($key)) {
+            $sql = "SELECT *
+                    FROM CENTRALE_PRODUITS.dbo.PRODUITS
+                    WHERE FO_ID = :id";
+            $conn = $connection->prepare($sql);
+            $conn->bindValue('id', $id);
+            $conn->execute();
+            $result = $conn->fetchAll();
+            if (!isset($result)) {
+                return new JsonResponse("Aucun produit trouvé", 200);}
+            $data = $helper->array_utf8_encode($result);
+            $id = $helper->getIdFromApiKey($key);
+//            $log->logAction($id[0]['APP_ID'], "get:produits");
+            return new JsonResponse($data, 200);
+        } else {
+            return new JsonResponse("Vous n'avez pas accès a ces ressources", 500);
+        }
+
+    }
+
 }
