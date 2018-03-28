1ead372891c97c4e03724b9274f408c688e804d4
josh-taylor-k3 <this14hawk@gmail.com>
2018-03-28 12:26:00 +0200

    modification fournisseur

--- src/Controller/FournisseurController.php
+++ src/Controller/FournisseurController.php
@@ -20,29 +20,45 @@ class FournisseurController extends Controller
     {
 
 
+
+
+
+
         header("Access-Control-Allow-Origin: *");
-
-
         $key = $request->headers->get('X-ac-key');
 
-        if (isset($key) && $auth->grant($key)) {
+        $limit = $request->query->get('limit');
 
-            $sql = "SELECT *
+        $grant = $auth->grant($key);
+
+        switch ($grant){
+            case $grant['profil'] == "FOURNISSEUR":
+                return new JsonResponse("Vous n'avez pas accès a ces ressources", 500);
+                break;
+
+            case $grant['profil'] == "CENTRALE":
+
+                $centrale = $grant['centrale'];
+
+                $sql = "SELECT *
                     FROM CENTRALE_PRODUITS.dbo.FOURNISSEURS";
 
-            $conn = $connection->prepare($sql);
-            $conn->execute();
-            $result = $conn->fetchAll();
-            if (!isset($result)) {
-                return new JsonResponse("Aucun produit trouvé", 200);
-            }
-            $data = $helper->array_utf8_encode($result);
-            $id = $helper->getIdFromApiKey($key);
+                $conn = $connection->prepare($sql);
+                $conn->execute();
+                $result = $conn->fetchAll();
+                if (!isset($result)) {
+                    return new JsonResponse("Aucun produit trouvé", 200);
+                }
+                $data = $helper->array_utf8_encode($result);
+                $id = $helper->getIdFromApiKey($key);
 //            $log->logAction($id[0]['APP_ID'], "get:produits");
-            return new JsonResponse($data, 200);
-        } else {
-            return new JsonResponse("Vous n'avez pas accès a ces ressources", 500);
+                return new JsonResponse($data, 200);
+
+                break;
         }
+
+
+
 
 
     }
@@ -53,32 +69,36 @@ class FournisseurController extends Controller
     public function getFournisseur(Request $request , ApiKeyAuth $auth, Connection $connection, HelperService $helper, $id)
     {
 
-
-
         header("Access-Control-Allow-Origin: *");
-
-
         $key = $request->headers->get('X-ac-key');
 
-        if (isset($key) && $auth->grant($key)) {
+        $limit = $request->query->get('limit');
 
-            $sql = "SELECT *
+        $grant = $auth->grant($key);
+
+        switch ($grant){
+            case $grant['profil'] == "FOURNISSEUR":
+                return new JsonResponse("Vous n'avez pas accès a ces ressources", 500);
+                break;
+
+            case $grant['profil'] == "CENTRALE":
+                $sql = "SELECT *
                     FROM CENTRALE_PRODUITS.dbo.FOURNISSEURS
                     WHERE FO_ID = :id";
 
-            $conn = $connection->prepare($sql);
-            $conn->bindValue('id', $id);
-            $conn->execute();
-            $result = $conn->fetchAll();
-            if (!isset($result)) {
-                return new JsonResponse("Aucun produit trouvé", 200);
-            }
-            $data = $helper->array_utf8_encode($result);
-            $id = $helper->getIdFromApiKey($key);
+                $conn = $connection->prepare($sql);
+                $conn->bindValue('id', $id);
+                $conn->execute();
+                $result = $conn->fetchAll();
+                if (!isset($result)) {
+                    return new JsonResponse("Aucun produit trouvé", 200);
+                }
+                $data = $helper->array_utf8_encode($result);
+                $id = $helper->getIdFromApiKey($key);
 //            $log->logAction($id[0]['APP_ID'], "get:produits");
-            return new JsonResponse($data, 200);
-        } else {
-            return new JsonResponse("Vous n'avez pas accès a ces ressources", 500);
+                return new JsonResponse($data, 200);
+
+                break;
         }
 
 
