5e5acd77066a16135857d01b97054749dbc46a90
josh-taylor-k3 <this14hawk@gmail.com>
2018-03-28 14:59:03 +0200

    debug api

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -25,7 +25,7 @@ class ClientController extends Controller
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Credentials: true ");
         header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-        header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control");
+        header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, X-ac-key");
         $key = $request->headers->get('X-ac-key');
 
         $limit = $request->query->get('limit');
