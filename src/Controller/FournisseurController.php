54b49f9976339cf2ac4c0d2e1b06411f491a75ed
josh-taylor-k3 <this14hawk@gmail.com>
2018-04-19 10:31:49 +0200

    test conso

--- src/Controller/FournisseurController.php
+++ src/Controller/FournisseurController.php
@@ -140,6 +140,14 @@ class FournisseurController extends Controller
     {
 
 
+
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Credentials: true ");
+        header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+        header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, X-ac-key");
+
+
         $sql = "SELECT FO_RAISONSOC, FO_ID
                     FROM CENTRALE_PRODUITS.dbo.FOURNISSEURS";
 
