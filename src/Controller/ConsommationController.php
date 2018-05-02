cac54a8c50e71a6cab1d363d67a56913305d0979
josh-taylor-k3 <this14hawk@gmail.com>
2018-05-02 16:05:20 +0200

    changement de method pour voir ca fonctionne

--- src/Controller/ConsommationController.php
+++ src/Controller/ConsommationController.php
@@ -147,6 +147,10 @@ class ConsommationController extends Controller
         ];
 
         header("Access-Control-Allow-Origin: http://secure.achatcentrale.fr/");
+        header('Content-Type: application/json;charset=UTF-8');
+        header('Access-Control-Allow-Methods: DELETE, HEAD, GET, OPTIONS, POST, PUT');
+        header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+        header('Access-Control-Max-Age: 1728000');
 
 
         return new JsonResponse($result, 200);
