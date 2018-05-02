cdee9e625b7c7e76f241e373c6b78dc6222bac41
josh-taylor-k3 <this14hawk@gmail.com>
2018-05-02 14:41:01 +0200

    changement du code pour la conso client

--- src/Controller/ConsommationController.php
+++ src/Controller/ConsommationController.php
@@ -37,7 +37,9 @@ class ConsommationController extends Controller
     public function consoClient(Connection $connection, HelperService $helper,$id, $start, $end)
     {
         header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: Content-Type, User-Agent, Cache-Control");
+        header("Access-Control-Allow-Credentials: true ");
+        header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+        header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, Cache-Control");
 
 
         $sqlBruneau = "SELECT CLC_ID, CL_ID, CC_ID, FO_ID, CLC_DATE, CLC_PRIX_PUBLIC, CLC_PRIX_CENTRALE, INS_DATE, INS_USER , (
