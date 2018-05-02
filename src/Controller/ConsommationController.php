272286bc99d2316ac6f1cc23ba85fc8faaca1927
josh-taylor-k3 <this14hawk@gmail.com>
2018-05-02 16:09:20 +0200

    changement de method pour voir ca fonctionne

--- src/Controller/ConsommationController.php
+++ src/Controller/ConsommationController.php
@@ -37,11 +37,7 @@ class ConsommationController extends Controller
     {
 
 
-        header("Access-Control-Allow-Origin: *");
-        header('Content-Type: application/json;charset=UTF-8');
-        header('Access-Control-Allow-Methods: DELETE, HEAD, GET, OPTIONS, POST, PUT');
-        header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
-        header('Access-Control-Max-Age: 1728000');
+
 
         $sqlBruneau = "SELECT CLC_ID, CL_ID, CC_ID, FO_ID, CLC_DATE, CLC_PRIX_PUBLIC, CLC_PRIX_CENTRALE, INS_DATE, INS_USER , (
                   case month(CLC_DATE)
