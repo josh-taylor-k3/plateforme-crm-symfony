552ca44b2e3ffc24de03701a043354971bfbec2d
josh-taylor-k3 <this14hawk@gmail.com>
2018-05-02 15:10:58 +0200

    changement du code pour la conso client

--- src/Controller/ConsommationController.php
+++ src/Controller/ConsommationController.php
@@ -39,7 +39,7 @@ class ConsommationController extends Controller
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Credentials: true ");
         header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
-        header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, X-ac-key");
+        header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, Cache-Control");
 
 
 
@@ -163,7 +163,10 @@ class ConsommationController extends Controller
 
         ];
 
-
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Credentials: true ");
+        header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
+        header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, Cache-Control");
 
         return new JsonResponse($result, 200);
 
