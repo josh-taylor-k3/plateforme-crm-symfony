8c5984fac2751bdafa57e98b477daaa919225711
josh-taylor-k3 <this14hawk@gmail.com>
2018-05-02 15:09:12 +0200

    changement du code pour la conso client

--- src/Controller/ConsommationController.php
+++ src/Controller/ConsommationController.php
@@ -37,7 +37,6 @@ class ConsommationController extends Controller
     public function consoClient(Connection $connection, HelperService $helper,$id, $start, $end)
     {
         header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Credentials: true ");
         header("Access-Control-Allow-Methods: OPTIONS, GET, POST");
         header("Access-Control-Allow-Headers: Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, X-ac-key");
